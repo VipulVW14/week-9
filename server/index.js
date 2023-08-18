15772e73ffd7bfdfccab0e8b4d11f09cbe9e2775
dee022 <133804910+dee022@users.noreply.github.com>
2023-08-18 20:59:58 +0530

    / endpoint

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ app.use("/user", userRouter)
 
 
 // Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
 // mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+  
 app.listen(3000, () => console.log('Server running on port 3000'));
